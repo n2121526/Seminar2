2e065e402a7b19eaeae6f8a3aee5994564563410
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-09-29 10:57:38 +0300

    Add Task1

---
+++ Task.py
@@ -0,0 +1,16 @@
+#Задача 1. Напишите программу, которая принимает на вход вещественное или целое число и показывает сумму его цифр. Через строку нельзя решать.
+
+n = str(input('Введите вещественное число: '))
+
+def Sum_num(n):
+    sp = list(n)
+    print(sp)
+    for element in sp:   
+        if element == ',': 
+            sp.remove(',')
+
+    summ = 0
+    for element in sp:    
+        summ += int(element)
+    return summ
+print(Sum_num(n))
